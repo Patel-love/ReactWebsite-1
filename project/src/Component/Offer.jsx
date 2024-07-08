1b35759330b576afd9fc06eeddacfa2edac802de
Manav <manavamrutiya29@gmail.com>
2024-07-08 08:45:08 +0530

    React Website

--- project/src/Component/Offer.jsx
+++ project/src/Component/Offer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import offer from "../assets/offer-img.png";
 import arr from '../assets/color-arrow.png'
+import choco from '../assets/sl-1.png'
+import choc from '../assets/sl-1.png'
 
 function Offer() {
   return (
     <>
       <div className="container mt-5 text-center position-relative">
-        <div className="card" style={{ width: " 100%", height:"440px"}}>
+        <div className="card" style={{ width: " 100%", height:"440px",border:"3px solid brown"}}>
           <div className="card-body">
             <h1 className="card-title m-5">Offers on chocolates</h1>
             <h3 className="card-text m-4">
@@ -24,6 +26,11 @@ function Offer() {
         <h5 className="mt-3"  ><a href="" style={{textDecoration:"none"}}><i className="text-dark">See More  <img src={arr} alt="" /></i></a></h5>
        
       </div>
+      <div className="container position-absolute" style={{ bottom: "-180%", }} >
+        <img src={choco} alt="" srcset="" style={{width:'300px', paddingLeft:"6%"}}  />
+       
+      </div>
+     
     </>
   );
 }
